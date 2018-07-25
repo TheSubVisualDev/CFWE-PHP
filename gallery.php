737cebe02f5733a531736c35a80daf1b98d07400
Louis Beauvois <thesubvisual@gmail.com>
2018-07-25 15:51:05 +0100

    End of Cycle

--- gallery.php
+++ gallery.php
@@ -2,34 +2,25 @@
 <html>
 <head>
 	<?php include 'menu.php';?>
-	
+      
+    </script>
 
 </head>
 <body>
 	<br>
-	<h3>Welcome to my website!</h3><br>
+	<h3>3D assets.</h3><br>
 
- <table width="100%" height="40%" border="0">
-    <tr><td colspan="2" bgcolor = "#222" border="0" bordercolor="#555"></td></tr>
-         <tr valign = "top">
-            <td bgcolor = "#222" width = "50" height="100%" border="0" bordercolor="#555">
-      <iframe id="preview1" align="left" width="100%" height="100%" src="https://sketchfab.com/models/dc8c7676b32340b8b2d83da8fe09ebad/embed?autostart=1&amp;transparent=1" onload="calcHeight();" frameborder="0" allow="autoplay; fullscreen; vr" mozallowfullscreen="true" webkitallowfullscreen="true"> </iframe>
-           </td>
-            
-            <td bgcolor = "#222" width = "50%" height = "100%" border="0" bordercolor="#555">
-      <iframe id"preview2" align="right" width="100%" height="100%" src="https://sketchfab.com/models/64e6dcf737424025932e0365eba777e0/embed?autostart=1&amp;transparent=1" onload="calcHeight();" frameborder="0" allow="autoplay; fullscreen; vr" mozallowfullscreen="true" webkitallowfullscreen="true"> </iframe>
-           </td>
-         </tr>
-  </table>
-  <table width="100%" height="1" border="0" bordercolor="#555">
-    <tr><td colspan="1" bgcolor="#222" border="0" bordercolor="#555">
-      <br>
-      <br>
-      <br>
-      <br>
-      </td></tr>
-  </table>
-  
+     <?php
+
+    $csv = str_getcsv(file_get_contents('artEmbeds.csv'));
+    foreach ($csv as $value) {
+        $x = $value;
+        $y = str_replace('"',"",$x);
+        echo "$y<br>";
+    }
+
+     ?>
+
 	<?php include 'sta_foot.php';?> <!-- adds footer with custom php scripted content to the bottom of the page. -->
 </body>
 </html>
--- index.php
+++ index.php
@@ -9,6 +9,12 @@
 	<br>
 	<h3>Welcome to my website!</h3><br>
 
+	<?php
+
+	$testingstrings = "<p>I am a testing string to be displayed using raw HTML formatting!</p>";
+	echo $testingstrings;
+
+	?>
 	<p>Welcome to my testbed website! I'll be posting a multitude of things on here, from small useful tools to aid DMs in D&D to 3D models!<br><br>Thank you for visiting my testbed.</p>
 	<?php include 'sta_foot.php';?> <!-- adds footer with custom php scripted content to the bottom of the page. -->
 </body>
