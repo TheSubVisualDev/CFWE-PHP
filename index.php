673fbf7c13393187a71fba33c6010d1cdb9ec579
Louis Beauvois <thesubvisual@gmail.com>
2018-07-25 16:59:47 +0100

    Pushed new asset.
    Replaced old parallax asset with a new render of an icosohedran.

--- index.php
+++ index.php
@@ -13,20 +13,5 @@
 	<p>Welcome to my testbed website! I'll be posting a multitude of things on here, from small useful tools to aid DMs in D&D to 3D models!<br><br>Thank you for visiting my testbed.</p>
 	<?php include 'sta_foot.php';?> <!-- adds footer with custom php scripted content to the bottom of the page. -->
 
-	<script>
-		window.onscroll = function() {myFunction()};
-
-		var header = document.getElementById("myHeader");
-		var sticky = header.offsetTop;
-
-		function myFunction() {
-		  if (window.pageYOffset >= sticky) {
-		    header.classList.add("sticky");
-		  } else {
-		    header.classList.remove("sticky");
-		  }
-		}
-</script>
-
 </body>
 </html>
